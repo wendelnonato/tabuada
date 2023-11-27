6f7ab03b59982fd737b368dd0496c88f58695dc7
wendelnonato <wendelnonato13@gmail.com>
2023-11-27 15:19:43 -0300

    tabuada

---
+++ Tabuada.py
@@ -0,0 +1,34 @@
+#Apresentação da Tabuada
+
+print('\n\t--Tabuada--')
+
+# Entrada
+
+num =int(input(f'\nDigite o numero para ver sua tabuada:'))
+print('-'* 38)
+
+# Processamento
+
+n1 = num * 1
+n2 = num * 2
+n3 = num * 3
+n4 = num * 4
+n5 = num * 5
+n6 = num * 6
+n7 = num * 7
+n8 = num * 8
+n9 = num * 9
+n10 = num * 10
+
+# Saida
+
+print(num,'X 1 =',n1)
+print(num,'X 2 =',n2)
+print(num,'X 3 =',n3)
+print(num,'X 4 =',n4)
+print(num,'X 5 =',n5)
+print(num,'X 6 =',n6)
+print(num,'X 7 =',n7)
+print(num,'X 8 =',n8)
+print(num,'X 9 =',n9)
+print(num,'X 10 =',n10)
